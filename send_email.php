d804402d1d6745ffbdd4a7ed2877be21d958be05
Marius Joerger <suiram.joerger@gmail.com>
2024-11-08 10:14:47 +0100

    add php file

---
+++ send_email.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Formulardaten abrufen und schützen
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // E-Mail-Zieladresse
+    $to = "info@connexa-gmbh.ch";
+    $subject = "Neue Kontaktanfrage von $name";
+    
+    // Nachricht formatieren
+    $body = "Name: $name\n";
+    $body .= "E-Mail: $email\n\n";
+    $body .= "Nachricht:\n$message\n";
+
+    // E-Mail-Header
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    // E-Mail senden
+    if (mail($to, $subject, $body, $headers)) {
+        echo "<p>Vielen Dank! Ihre Nachricht wurde gesendet.</p>";
+    } else {
+        echo "<p>Entschuldigung, Ihre Nachricht konnte nicht gesendet werden. Versuchen Sie es später erneut.</p>";
+    }
+}
+?>
